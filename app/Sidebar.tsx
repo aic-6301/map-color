8672c402ab5063b654891cc49c88c2e1e33b79cd
aic-6301 <rx435423@gmail.com>
2024-09-16 10:57:04 +0900

    react

--- app/Sidebar.tsx
+++ app/Sidebar.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import React, { useState } from 'react';
 import { Checkbox, FormControl, InputLabel, MenuItem, Select, TextField, List, ListItem, ListItemText, Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, IconButton, ListItemButton } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -139,4 +138,3 @@ const Sidebar: React.FC<SidebarProps> = ({
 };
 
 export default Sidebar;
-/* eslint-enable */
